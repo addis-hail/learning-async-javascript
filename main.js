6f7c5524778cdc122515a65956e03fd201ab8b23
addis-hail <addishailegnaw@yahoo.com>
2022-09-15 14:04:43 +0300

    async & await

--- main.js
+++ main.js
@@ -1,10 +1,29 @@
-// fetch api
+// async & await
 
-fetch('todos/addis.json').then((response) => {
-    console.log('resolved', response);
-    return response.json();
-}).then(data => {
-    console.log(data);
-}).catch(() => {
-    console.log('rejected', err);
-});
+const getTodos = async () => {
+
+    const response = await fetch('todos/addis.json');
+    const data = await response.json();
+    return data;
+
+};
+
+console.log(1);
+console.log(2);
+
+getTodos()
+.then(data => console.log('resolved:', data));
+
+console.log(3);
+console.log(4);
+
+
+
+// fetch('todos/addis.json').then((response) => {
+//     console.log('resolved', response);
+//     return response.json();
+// }).then(data => {
+//     console.log(data);
+// }).catch(() => {
+//     console.log('rejected', err);
+// });

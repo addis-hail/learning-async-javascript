1a255dff43f1448964b0f83be09451e60aa5c518
addis-hail <addishailegnaw@yahoo.com>
2022-09-15 14:12:52 +0300

    throwing errors

--- main.js
+++ main.js
@@ -2,20 +2,21 @@
 
 const getTodos = async () => {
 
-    const response = await fetch('todos/addis.json');
+    const response = await fetch('todos/addiss.json');
+
+    if(response.status !== 200){
+        throw new Error('cannot fetch the data');
+    }
+
     const data = await response.json();
     return data;
 
 };
 
-console.log(1);
-console.log(2);
-
 getTodos()
-.then(data => console.log('resolved:', data));
+.then(data => console.log('resolved:', data))
+.catch(err => console.log('rejected:', err.message));
 
-console.log(3);
-console.log(4);
 
 
 
